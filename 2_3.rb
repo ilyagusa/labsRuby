75f3b5d6299231d8e13efe3e77f8c72eed59cbca
ilyagusa <48081482+ilyagusa@users.noreply.github.com>
2019-03-01 23:22:46 +0300

    add 2_3.rb

--- 2_3.rb
+++ 2_3.rb
@@ -1,48 +1,64 @@
+def chekName(nameSurn,exp)
+        if ((nameSurn.chomp()=="Petr Petrovich") && (exp>5 && exp<15) )
+                puts "Профессия:Заслуженный Руководитель"
+        elsif ((nameSurn.chomp()=="Petr Petrovich") && exp>15)
+                puts "Профессия:Известный Руководитель"
+        elsif((nameSurn.chomp()=="Petr Petrovich") && exp.to_i<=5)  
+                puts "Профессия:Руководитель"
+        else 
+                return puts "debil"
+               exit
+        end
+end
+
+
 def getStr()
-    str=gets
-    if str==nil
-        return nil
+        str=gets
+        if str==nil
+            return nil
+            exit
+    else return str
+    end
+end
+    
+    def getAge()
+        age=gets
+        return nil if age.nil?
+        age=Integer(age)
+        if  age >= 15 and age <= 100
+         age
+        else 
+        print "invalid Age"
         exit
-else return str
-end
-end
-
-def getAge()
-    age=gets
-    return nil if age.nil?
-    age=Integer(age)
-    if  age >= 15 and age <= 100
-    return age
-    else 
-    print "invalid Age"
-    exit
+        end
     end
-end
-
-
-def getExp()
-    exp=gets
-    return nil if exp.nil?
-    exp=Integer(exp)
-    if exp>0  
-    return Integer(exp)
-    else
-    puts "invalid expirience"
-    exit
+    
+    
+    def getExp()
+        exp=gets
+        return nil if exp.nil?
+        exp=Integer(exp)
+        if exp>0  
+        return Integer(exp)
+        else
+        puts "invalid expirience"
+        exit
+        end
     end
-end
+    
+    
+    
+    print "Enter name and surname>"
+    NameAndSurname=getStr()
+    puts "Name and Surname:#{NameAndSurname.chomp}"
+    print "Enter your mail>"
+    Mail=getStr()
+    puts "Mail:#{Mail.chomp}"
+    print "Enter your age>"
+    Age=getAge()
+    puts "Age:#{Age}"
+    print "Enter your work exp>"
+    Expirience=getExp()
+    puts "Exp:#{Expirience}"
 
-
-
-print "Enter name and surname>"
-NameAndSurname=getStr()
-puts "Name and Surname:#{NameAndSurname.chomp}"
-print "Enter your mail>"
-Mail=getStr()
-puts "Mail:#{Mail.chomp}"
-print "Enter your age>"
-Age=getAge()
-puts "Age:#{Age}"
-print "Enter your work exp>"
-Expirience=getExp()
-puts "Exp:#{Expirience}"
+    chekName(NameAndSurname,Expirience)
